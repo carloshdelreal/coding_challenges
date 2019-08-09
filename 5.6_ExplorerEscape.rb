7f52649f746894e9744f8e3504e8286d341106e2
Carlos Del Real <carloshdelreal@gmail.com>
2019-08-08 23:44:30 -0500

    finished 5.6 explorer scape

--- 5.6_ExplorerEscape.rb
+++ 5.6_ExplorerEscape.rb
@@ -1,16 +1,15 @@
 def get_nodes(maze, node=[0,0])
 	nodes = []
-	node_down = [node[0]+1,node[1]]
-	node_right = [node[0], node[1]+1]
-	#puts "Node: #{node} N_Down: #{node_down}, N_Right: #{node_right}"
+	node_right = [node[0]+1,node[1]]
+	node_down = [node[0], node[1]+1]
 	
 	#node down
-	if maze[0].length > node[0]+1
+	if maze.length > node[1]+1
 		#down
-		nodes.push(node_down) if maze[node[0]+1][node[1]] != 1 && maze[node[0]+1][node[1]] != nil
+		nodes.push(node_down) if maze[node[1]+1][node[0]] != 1 && maze[node[1]+1][node[0]] != nil
 	end
-	if maze[0].length > node[1]
-		nodes.push(node_right) if maze[node[0]][node[1]+1] != 1 && maze[node[0]][node[1]+1] != nil
+	if maze[0].length > node[0]
+		nodes.push(node_right) if maze[node[1]][node[0]+1] != 1 && maze[node[1]][node[0]+1] != nil
 	end
 	nodes
 end
@@ -19,19 +18,23 @@ end
 def maze_escape(maze)
 	# write your code here
 	start = [0,0]
-	visited = []
-	
+
 	q = Queue.new
-	q.push(start)
+	q.push([start])
 	while q.length > 0
 		poped = q.pop
-		visited.push(poped)
-		nodes = get_nodes(maze, poped)
+
+		nodes = get_nodes(maze, poped[-1])
 		nodes.each do |n|
-			q.push(n)
+			if maze[n[1]][n[0]] == 9
+				poped.push(n)	
+				return poped
+			end
+			c = poped.clone
+			c.push(n)
+			q.push(c)
 		end
 	end
-	visited.to_s
 end
   
 p maze_escape(
@@ -43,31 +46,21 @@ p maze_escape(
 		[0, 0, 0, 1, 9]
 	]
 )
-# => [ 
-#      [0, 0],
-#      [1, 0],
-#      [2, 0],
-#      [2, 1],
-#      [2, 2],
-#      [2, 3],
-#      [3, 3],
-#      [4, 3],
-#      [4, 4]
-#    ]
-
-# maze =
-# 	[
-# 		[0, 0, 0, 0, 0], 
-# 		[0, 1, 0, 1, 0], 
-# 		[0, 1, 0, 1, 1], 
-# 		[0, 1, 0, 0, 0], 
-# 		[0, 0, 0, 1, 9]
-# 	]
 
 
+# => [[0, 0],[1, 0],[2, 0],[2, 1],[2, 2],[2, 3],[3, 3],[4, 3],[4, 4]]
 
-# puts get_nodes(maze, node=[0,4]).to_s
-# puts "[[1, 4]]"
-# puts get_nodes(maze, node=[4,0]).to_s
-# puts "[[4, 1]]"
-#puts maze[2][4]
+maze =
+	[
+		[0, 0, 0, 0, 0], 
+		[0, 1, 0, 1, 0], 
+		[0, 1, 0, 1, 1], 
+		[0, 1, 0, 0, 0], 
+		[0, 0, 0, 1, 9]
+	]
+
+
+p maze_escape([	[0, 0, 0], 
+				[0, 1, 9], 
+				[0, 0, 0]])
+# expected: [[0, 0], [1, 0], [2, 0], [2, 1]]
