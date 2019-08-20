cb4b8c709294f431d467100a334fe6e83ef58b60
Carlos Del Real <carloshdelreal@gmail.com>
2019-08-20 00:49:46 -0500

    7.3 insertion sort algorithm complete

---
+++ 7.3_insertionself.rb
@@ -0,0 +1,25 @@
+def sort_itself(array)
+	# write your code here
+	print_bool = false
+	array.each_with_index do |item, index|
+		loop do
+			if item > array[index-1] || index ==0
+				array[index] = item
+				break
+			end
+			array[index] = array[index-1]
+			index -= 1
+		end
+		puts array.join(" ") if print_bool
+		print_bool = true
+	end
+end
+
+sort_itself([1, 4, 3, 6, 9, 2])
+# => 1 4 3 6 9 2
+#    1 3 4 6 9 2
+#    1 3 4 6 9 2
+#    1 3 4 6 9 2
+#    1 2 3 4 6 9
+
+sort_itself([9, 8, 6, 7, 3, 5, 4, 1, 2])
