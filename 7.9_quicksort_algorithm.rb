39b4aabf49567a980d3afae1a163b6d94b3c44ac
Carlos Del Real <carloshdelreal@gmail.com>
2019-08-22 11:46:00 -0500

    7.9 Quick sort algorithm, part 1

---
+++ 7.9_quicksort_algorithm.rb
@@ -0,0 +1,15 @@
+def partition(array)
+	# write your code here
+	
+	pivot = array.delete_at(0)
+	left = []
+	right = []
+	array.each do |item|
+		pivot > item ? left << item : right << item
+	end
+	left + [pivot] + right
+
+end
+
+p partition([4, 5, 3, 9, 1])
+# => [3, 1, 4, 5, 9]
