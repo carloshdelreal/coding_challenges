f65250c071c5cf62714d1f616050dea10cf45bd0
Carlos Del Real <carloshdelreal@gmail.com>
2019-09-06 11:13:06 -0500

    8.3 index match

---
+++ 8.3_Index_match.rb
@@ -0,0 +1,10 @@
+def index_match(array, start = 0, finish = array.length)
+  guess = (finish + start) / 2
+  return guess if array[guess] == guess
+  return -1 if start == finish || start == guess
+  array[guess] > guess ? index_match(array,start,guess) :
+                       index_match(array,guess,finish)
+end
+
+puts index_match([0, 2, 3, 7, 9, 11])
+# => 0
