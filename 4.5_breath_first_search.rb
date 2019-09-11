c0a390ee88a961d82c08ba61700e344d142ee9fc
Carlos Del Real <carloshdelreal@gmail.com>
2019-09-11 16:17:47 -0500

    4.5 breath first seach, recursive

---
+++ 4.5_breath_first_search.rb
@@ -0,0 +1,45 @@
+def bfs(graph, start=0)
+  # write your code here
+  q = Queue.new 
+  visited = []
+  q.push(start)
+  while q.length > 0
+    p = q.pop
+    (visited.include? p) ? next : visited.push(p)
+    
+    nodes = graph[p]
+    nodes.each {|x| q.push(x)}
+  end
+  return visited
+end
+
+def recursive_bfs(graph,parents = [0], visited = [])
+  return visited if parents.length <=0
+  visited += parents
+  level_nodes = []
+  parents.each do |parent|
+    level_nodes += graph[parent]
+  end
+  level_nodes = level_nodes.select{|x| !(visited.include? x)}
+  recursive_bfs(graph, level_nodes, visited)
+  
+end
+
+p bfs({
+  0 => [2], 
+  1 => [4], 
+  2 => [5, 0, 3], 
+  3 => [2], 
+  4 => [1, 5], 
+  5 => [4, 2]
+})
+#=> [0, 2, 5, 3, 4, 1]
+
+p recursive_bfs({
+  0 => [2], 
+  1 => [4], 
+  2 => [5, 0, 3], 
+  3 => [2], 
+  4 => [1, 5], 
+  5 => [4, 2]
+  })
