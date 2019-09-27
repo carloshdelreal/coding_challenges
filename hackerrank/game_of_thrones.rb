167efb6a1abf361a149ca384e8e3df00a8107844
Carlos Del Real <carloshdelreal@gmail.com>
2019-09-27 10:57:19 -0500

    Game of thrones counting letters

---
+++ hackerrank/game_of_thrones.rb
@@ -0,0 +1,18 @@
+def gameOfThrones(s)
+  count_hash = {}
+  s.split('').each do |letter|
+    count_hash[letter] += 1 if count_hash[letter]
+    count_hash[letter] = 1 unless count_hash[letter]
+  end
+  puts count_hash.to_s
+  if s.length.even?
+    count_hash.values.select{|x| x.odd? }.length < 1 ? 'YES' : 'NO'
+  else
+    count_hash.values.select{|x| x.odd? }.length == 1 ? 'YES' : 'NO'
+  end
+
+end
+
+p gameOfThrones('ddcceefeeccdd')
+p gameOfThrones('cdefghmnopqrstuvw')
+p gameOfThrones('aaabbbb')
