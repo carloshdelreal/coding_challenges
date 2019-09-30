87ba60d2464a121143c1b1e1e937a0a57d459d16
Carlos Del Real <carloshdelreal@gmail.com>
2019-09-30 11:22:51 -0500

    finish anagram

--- hackerrank/anagram.rb
+++ hackerrank/anagram.rb
@@ -1,11 +1,29 @@
 def anagram(s)
   return -1 if s.length.odd?
+  
   s = s.split('')
   start = s.slice(0, s.length/2)
   final = s.slice(s.length/2, s.length)
-  start.difference(final).length
+  count_hash = {}
+  start.each do |letter|
+    count_hash[letter] += 1 if count_hash[letter]
+    count_hash[letter] = 1 unless count_hash[letter]
+  end
+  final.each do |letter|
+    count_hash[letter] -= 1 if count_hash[letter]
+    count_hash[letter] = -1 unless count_hash[letter]
+  end
   
-
+  result = [0,0]
+  count_hash.values.each do |x|
+    next if x.zero?
+    if x.positive?
+      result[0] += x
+    else
+      result[1] += x
+    end
+  end
+  result[0].abs == result[1].abs ? result[0] : -1
 end
 
 p anagram('aaabbb')
@@ -17,6 +35,7 @@ p anagram('xaxbbbxx')
 p anagram('asdfjoieufoa')
 p anagram('fdhlvosfpafhalll')
 p anagram('mvdalvkiopaufl')
+
 
 # expected
 # 3
