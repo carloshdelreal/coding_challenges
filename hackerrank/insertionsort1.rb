596c6744259e9fffc7fd165a3692dc51bc4234c4
Carlos Del Real <carloshdelreal@gmail.com>
2019-10-03 12:08:24 -0500

    finish hackerrank insertionsort 1

---
+++ hackerrank/insertionsort1.rb
@@ -0,0 +1,13 @@
+def insertionSort1(n, arr)
+  temp = arr[-1]
+  (arr.length-1).downto(0).each do |i|
+    arr[i] = arr[i-1] if i > 0
+    if arr[i] < temp || i == 0
+      arr[i] = temp
+      break
+    end
+    puts arr.join(' ')
+  end
+  puts arr.join(' ')
+end
+p insertionSort1(2, [2, 4, 6, 8, 3])
