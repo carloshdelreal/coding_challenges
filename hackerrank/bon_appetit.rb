4d7b852a0c0cb89b063efc57eeba5c4d8793a7dc
Carlos Del Real <carloshdelreal@gmail.com>
2019-10-07 10:30:36 -0500

    finish hackerrank bonappettit

---
+++ hackerrank/bon_appetit.rb
@@ -0,0 +1,17 @@
+
+
+def bon_appetit(bill, k, b)
+  ana_pay = (bill.sum - bill[k]) / 2
+  if ana_pay == b
+    puts 'Bon Appetit'
+  else
+    puts b - ana_pay
+  end
+end
+
+k = 1
+bill = [3, 10, 2, 9]
+b = 12
+
+p bon_appetit(bill, k, b)
+
