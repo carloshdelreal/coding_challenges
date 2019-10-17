c756ac0d28bc6172603e6ece46a39e864e71cda8
Carlos Del Real <carloshdelreal@gmail.com>
2019-10-17 16:09:56 -0500

    palindrome stupid and slow

---
+++ leet/5_palindrome.rb
@@ -0,0 +1,44 @@
+def palindrome?(a, i, j)
+  return true if j == i 
+  start = i
+  finish = j
+	loop do
+		break if i>=j
+		return false if a[i] != a[j]
+		i += 1
+		j -= 1
+  end
+  # puts "Palindrome: #{a[start..finish]}, i: #{start}, j: #{finish}"
+	return true
+end
+
+def longest_palindrome(s)
+  pal_len = 0
+  pal_s = ''
+  l = s.length
+  a = s.split('')
+  (0..l-1).each do |i|
+    (i..l).each do |j|
+      if palindrome?(a, i, j)
+        if a[i..j].length > pal_len
+          # puts "i: #{i}, j:#{j}"
+          pal_len = a[i..j].length
+          pal_s = a[i..j].join('')
+          # puts "Subs: #{a[i..j].join('')}, Leng: #{a[i..j-1].length}"
+        end
+      end
+    end
+  end
+  pal_s
+end
+
+p longest_palindrome('aaabbb')
+p longest_palindrome('aaabbbb')
+p longest_palindrome('babad')
+p longest_palindrome("ac")
+p longest_palindrome("a")
+# a = 'babad'.split('')
+# p palindrome?(a , 0, 2)
+# p a.slice(0,2).join('')
+# p a.slice(0,2)
+# p palindrome?('ac', 0, 1)
