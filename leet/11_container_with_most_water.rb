77f72ff52bed46dedccd6d41f2aadce79e337c7e
Carlos Del Real <carloshdelreal@gmail.com>
2019-10-17 16:30:24 -0500

    finish 11 container with most water

--- leet/11_container_with_most_water.rb
+++ leet/11_container_with_most_water.rb
@@ -1,4 +1,4 @@
-def max_area(height)
+def max_area_logn(height)
   max_a = 0
   finish = height.length - 1
   (0..finish-1).each do |i|
@@ -9,5 +9,26 @@ def max_area(height)
   end
   max_a
 end
+
+def max_area(height)
+  max_a = 0
+  start = 0
+  finish = height.length - 1
+  loop do
+    break if finish <= start
+    distance = finish - start
+    sample = [height[start], height[finish]].min * distance
+    max_a = sample if sample > max_a
+    if height[start] > height[finish]
+      finish -= 1
+    else
+      start += 1
+    end
+  end
+  max_a
+end
+
+
+
 
 p max_area([1,8,6,2,5,4,8,3,7])
