72034c9a53d7962d4c93b51c3dd6cabd6c31c266
Carlos Del Real <carloshdelreal@gmail.com>
2019-10-30 11:23:22 -0500

    generate parenthesis

--- leet/10_regular_expresion.rb
+++ leet/10_regular_expresion.rb
@@ -3,7 +3,7 @@ def is_match(s, p)
   j = 0
   loop do
       puts "letter #{s[i]} expresion #{p[j]}"
-      sleep(0.3)
+      
       if s[i].nil? || p[j].nil?
           break
       elsif s[i] == p[j] || p[j] == '.'
@@ -18,16 +18,44 @@ def is_match(s, p)
       else 
         if p[j+2].nil?
           return false
-        elsif p[j+1] == '*' 
+        elsif p[j+1] == '*'
           j += 2
         end
       end        
   end
-  return (p[j+1].nil? ? (s[i].nil? ? true : false) : false)
+  remainder = p.slice(j..-1)
+  puts "Remainder: #{remainder}, expression: #{p}"
+
+  if p[j].nil? && s[i].nil?
+    return true
+  elsif remainder
+    if p[j+1].nil? && p[j] == "*" || p[j+1] == "*"
+      return true
+    else
+      skip = false
+      remainder.each_char do |x|
+        puts x
+        unless skip
+          skip = true unless x == s[-1]
+        else
+          skip = false
+          return false unless x == "*"
+        end
+      end     
+      return skip ? false : true
+    end
+  else
+    return false
+  end
 end
 
-p is_match("aa", "a")
-p is_match("a", "a*")
-p is_match("ab", ".*")
-p is_match("aab", "c*a*b")
-p is_match("missisippi", "mis*is*p*.")
+# p is_match("aa", "a") # false
+# p is_match("a", "a*") # true
+# p is_match("ab", ".*") # true
+# p is_match("aab", "c*a*b") #true
+# p is_match("missisippi", "mis*is*p*.") #false
+p is_match("aaa", "aaaa") #false
+# p is_match("aaa", "a*a") # true
+# p is_match("aaa", "ab*a*c*a") # true
+# p is_match("ab", ".*c") # false
+# p is_match("a", "ab*") # true
