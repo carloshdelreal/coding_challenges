b4701eaf5213a98f162ca3567717c8032220a71b
Carlos Del Real <carloshdelreal@gmail.com>
2019-11-18 08:34:41 -0500

    fix rspec failing sum of three

--- leet/15_3sum.rb
+++ leet/15_3sum.rb
@@ -104,6 +104,22 @@ class SumOf3
     solutions
   end
 
+  def three_sum(nums)
+    solutions = []
+    finish = nums.length - 1
+    (0..finish - 2).each do |i|
+      ((i + 1)..(finish - 1)).each do |j|
+        ((j + 1)..finish).each do |k|
+          if (nums[i] + nums[j] + nums[k]).zero?
+            triplet = [nums[i], nums[j], nums[k]].sort
+            solutions << triplet unless solutions.include? triplet
+          end
+        end
+      end
+    end
+    solutions
+  end
+
   def three_sum_1(nums)
     solutions = []
     finish = nums.length - 1
